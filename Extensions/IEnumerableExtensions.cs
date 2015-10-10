e03f5e84297da906fdcdd9b14613a7ce0b3f35ba
Ömer Cinbat <ocinbat@outlook.com>
2015-10-10 16:29:22 +0300

    Frakework version downgrade from 4.6 to 4.

--- Extensions/IEnumerableExtensions.cs
+++ Extensions/IEnumerableExtensions.cs
@@ -1,16 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 public static class IEnumerableExtensions
 {
-    public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
-    {
-        HashSet<TKey> seenKeys = new HashSet<TKey>();
-
-        return source.Where(element => seenKeys.Add(keySelector(element)));
-    }
-
     public static bool IsEmpty<T>(this List<T> source)
     {
         return source == null || !source.Any();
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Jarvis")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("tr-TR")]
+[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyVersion("1.1.4.0")]
+[assembly: AssemblyFileVersion("1.1.4.0")]
