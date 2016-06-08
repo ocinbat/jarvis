0d6e0c185e2cf80aba97c30bdbbcb47deaf8ac7c
Ömer Cinbat <ocinbat@outlook.com>
2016-06-09 00:35:11 +0300

    Solution structure update.

---
+++ Jarvis.Tests/IEnumerableExtensionTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using Ploeh.AutoFixture;
+
+namespace Jarvis.Tests
+{
+    public class IEnumerableExtensionTests
+    {
+        [Test]
+        public void PartitionTest()
+        {
+            IEnumerable<int> sourceList = new Fixture().CreateMany<int>(56);
+
+            IEnumerable<IEnumerable<int>> lists = sourceList.Partition(5);
+
+            sourceList.Count().ShouldBeEquivalentTo(lists.Sum(l => l.Count()));
+        }
+    }
+}
---
+++ Jarvis/Extensions/GenericExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class GenericExtensions
+{
+    public static bool IsDefault<T>(this T value) where T : struct
+    {
+        bool isDefault = value.Equals(default(T));
+
+        return isDefault;
+    }
+}
