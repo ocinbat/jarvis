acda9eb65391205edd9907a04936e58cc7af900e
Ömer Cinbat <ocinbat@outlook.com>
2016-06-09 00:28:48 +0300

    IEnumerable Partition extension implementation.

--- Extensions/IEnumerableExtensions.cs
+++ Extensions/IEnumerableExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 public static class IEnumerableExtensions
@@ -21,5 +23,48 @@ public static class IEnumerableExtensions
     public static bool HasElements<T>(this IEnumerable<T> source)
     {
         return !IsEmpty(source);
+    }
+
+    /// <summary>
+    /// Taken from https://github.com/elastic/elasticsearch-net-example/blob/master/src/NuSearch.Domain/Extensions/PartitionExtension.cs
+    /// </summary>
+    /// <typeparam name="T">Generic IEnumerable type</typeparam>
+    /// <param name="source">source</param>
+    /// <param name="size">each partition size</param>
+    /// <returns>a list of partitions</returns>
+    public static IEnumerable<IEnumerable<T>> Partition<T>(this IEnumerable<T> source, int size)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException("source", "source cannot be null.");
+        }
+
+        T[] array = null;
+        int count = 0;
+
+        foreach (T item in source)
+        {
+            if (array == null)
+            {
+                array = new T[size];
+            }
+
+            array[count] = item;
+
+            count++;
+
+            if (count == size)
+            {
+                yield return new ReadOnlyCollection<T>(array);
+                array = null;
+                count = 0;
+            }
+        }
+
+        if (array != null)
+        {
+            Array.Resize(ref array, count);
+            yield return new ReadOnlyCollection<T>(array);
+        }
     }
 }
