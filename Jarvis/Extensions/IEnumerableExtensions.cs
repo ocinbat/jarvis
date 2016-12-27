09988c3e4ddd96f916c47eb018a0e11bfaa74854
Ömer Cinbat <ocinbat@outlook.com>
2016-12-27 17:02:52 +0300

    Fixed filtering namespace issue.

--- Jarvis/Extensions/IEnumerableExtensions.cs
+++ Jarvis/Extensions/IEnumerableExtensions.cs
@@ -126,41 +126,4 @@ public static class IEnumerableExtensions
         // Compile to Func<Data, Data>.
         return source.Select(lambda.Compile());
     }
-
-    public static IEnumerable<T> Where<T>(this IEnumerable<T> source, params QueryParameter[] queryParameters)
-    {
-        if (queryParameters.HasElements())
-        {
-            foreach (QueryParameter parameter in queryParameters)
-            {
-                if (parameter.Value != null)
-                {
-                    ParameterExpression param = Expression.Parameter(typeof(T), "p");
-
-                    Expression<Func<T, bool>> exp;
-
-                    Type parameterType = Expression.Property(param, parameter.Name).Type;
-
-                    if (parameterType.IsGenericType && parameterType.GetGenericTypeDefinition() == typeof(Nullable<>))
-                    {
-                        throw new NotImplementedException("Query parameter binding for nullable types are not implemented yet.");
-                    }
-                    else
-                    {
-                        exp = Expression.Lambda<Func<T, bool>>(
-                            Expression.Equal(
-                                Expression.Property(param, parameter.Name),
-                                Expression.Constant(parameter.Value)
-                            ),
-                            param
-                        );
-                    }
-
-                    source = source.Where(exp.Compile());
-                }
-            }
-        }
-
-        return source;
-    }
 }
